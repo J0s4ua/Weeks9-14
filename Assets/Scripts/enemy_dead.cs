d2abcecc766f79329d7e0c33c1b353d65fa179ad
Joshua <liangjos@sheridancollege.ca>
2025-04-06 19:22:06 -0400

    Added rest of sprites

---
+++ Assets/Scripts/enemy_dead.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_dead : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float timer = 0.2f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= 1f * Time.deltaTime;
+
+        if (timer <= 0) { 
+        
+            Destroy(gameObject);
+        
+        
+        }
+    }
+}
